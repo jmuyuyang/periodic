91fa8ed94be8d617796c72a60f312128a87c0e99
yuyang <yuyang@huanleguang.com>
2017-06-19 03:51:11 +0000

    修复cron job的bug

--- driver/job.go
+++ driver/job.go
@@ -2,24 +2,63 @@ package driver
 
 import (
 	"encoding/json"
+	"strings"
+	"time"
+
+	"github.com/Lupino/periodic/util"
+	"github.com/gorhill/cronexpr"
 )
 
 // Job workload.
 type Job struct {
-	ID int64 `json:"job_id"`
-	// The job name, this is unique.
-	Name string `json:"name"`
-	// The job function reffer on worker function
-	Func string `json:"func"`
-	// Job args
-	Args string `json:"workload"`
-	// Job processing timeout
-	Timeout int64 `json:"timeout"`
-	// When to sched the job.
-	SchedAt int64 `json:"sched_at"`
-	// The job is start at
-	RunAt  int64  `json:"run_at"`
-	Status string `json:"status"`
+	ID      int64         `json:"job_id"`
+	Name    string        `json:"name"`     // The job name, this is unique.
+	Func    string        `json:"func"`     // The job function reffer on worker function
+	Args    string        `json:"workload"` // Job args
+	Timeout int64         `json:"timeout"`  // Job processing timeout
+	SchedAt int64         `json:"sched_at"` // When to sched the job.
+	Period  string        `json:"period"`
+	RunAt   int64         `json:"run_at"`  // The job is start at
+	Counter int64         `json:"counter"` // The job run counter
+	Status  string        `json:"status"`
+	timeCon timeCondition `json:"_"`
+}
+
+type timeCondition struct {
+	Cron  *cronexpr.Expression
+	Every time.Duration
+}
+
+func (job *Job) Init() error {
+	if job.Period != "" {
+		if strings.Index(job.Period, "every_") == 0 {
+			every, err := util.ParseDuration(strings.Trim(job.Period[6:], " "))
+			if err != nil {
+				return err
+			}
+			job.timeCon = timeCondition{
+				Every: every,
+			}
+			job.SchedAt = time.Now().Add(every).Unix()
+		} else {
+			cron, err := cronexpr.Parse(job.Period)
+			if err != nil {
+				return err
+			}
+			job.timeCon = timeCondition{
+				Cron: cron,
+			}
+			job.SchedAt = cron.Next(time.Now()).Unix()
+		}
+	}
+	return nil
+}
+
+func (job Job) IsPeriod() bool {
+	if job.Period != "" {
+		return true
+	}
+	return false
 }
 
 // IsReady check job status ready
@@ -30,6 +69,17 @@ func (job Job) IsReady() bool {
 // IsProc check job status processing
 func (job Job) IsProc() bool {
 	return job.Status == "processing"
+}
+
+func (job *Job) ResetPeriod() {
+	if job.Period != "" {
+		schedTime := time.Unix(job.SchedAt, 0)
+		if job.timeCon.Cron == nil {
+			job.SchedAt = schedTime.Add(job.timeCon.Every).Unix()
+		} else {
+			job.SchedAt = job.timeCon.Cron.Next(schedTime).Unix()
+		}
+	}
 }
 
 // SetReady set job status ready
@@ -45,6 +95,9 @@ func (job *Job) SetProc() {
 // NewJob create a job from json bytes
 func NewJob(payload []byte) (job Job, err error) {
 	err = json.Unmarshal(payload, &job)
+	if err == nil {
+		err = job.Init()
+	}
 	return
 }
 
---
+++ util/time.go
@@ -0,0 +1,90 @@
+package util
+
+import (
+	"errors"
+	"strconv"
+	"time"
+)
+
+var ErrInvalidDuration = errors.New("invalid duration")
+
+func isDigit(ch rune) bool { return (ch >= '0' && ch <= '9') }
+
+// split splits a string into a slice of runes.
+func split(s string) (a []rune) {
+	for _, ch := range s {
+		a = append(a, ch)
+	}
+	return
+}
+
+func ParseDuration(s string) (time.Duration, error) {
+	// Return an error if the string is blank or one character
+	if len(s) < 2 {
+		return 0, ErrInvalidDuration
+	}
+
+	// Split string into individual runes.
+	a := split(s)
+
+	// Start with a zero duration.
+	var d time.Duration
+	i := 0
+
+	// Check for a negative.
+	isNegative := false
+	if a[i] == '-' {
+		isNegative = true
+		i++
+	}
+
+	// Parsing loop.
+	for i < len(a) {
+		// Find the number portion.
+		start := i
+		for ; i < len(a) && isDigit(a[i]); i++ {
+			// Scan for the digits.
+		}
+
+		// Check if we reached the end of the string prematurely.
+		if i >= len(a) || i == start {
+			return 0, ErrInvalidDuration
+		}
+
+		// Parse the numeric part.
+		n, err := strconv.ParseInt(string(a[start:i]), 10, 64)
+		if err != nil {
+			return 0, ErrInvalidDuration
+		}
+
+		// Extract the unit of measure.
+		// If the last two characters are "ms" then parse as milliseconds.
+		// Otherwise just use the last character as the unit of measure.
+		switch a[i] {
+		case 'u', 'µ':
+			d += time.Duration(n) * time.Microsecond
+		case 'm':
+			if i+1 < len(a) && a[i+1] == 's' {
+				d += time.Duration(n) * time.Millisecond
+				i += 2
+				continue
+			}
+			d += time.Duration(n) * time.Minute
+		case 's':
+			d += time.Duration(n) * time.Second
+		case 'h':
+			d += time.Duration(n) * time.Hour
+		case 'd':
+			d += time.Duration(n) * 24 * time.Hour
+		case 'w':
+			d += time.Duration(n) * 7 * 24 * time.Hour
+		default:
+			return 0, ErrInvalidDuration
+		}
+		i++
+	}
+	if isNegative {
+		d = -d
+	}
+	return d, nil
+}
